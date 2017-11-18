98f2951b985de725782bf461b5a47ac6086c11b7
Unknown <etienne.langensiepen@gmx.de>
2017-11-18 13:31:54 +0100

    Exc6_3

---
+++ Exc6/src/Exc6_3.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Exc6_3 {
+    public static void main(String[]args){
+        Scanner keyboard = new Scanner(System.in);
+        String place;
+        String status;
+
+        System.out.println("Think of something and i'll try to guess it!");
+        System.out.print("\nQuestion 1) Does it stay inside or outside or both? > ");
+        place = keyboard.next();
+        System.out.print("\nQuestion 2) Is it a living thing? > ");
+        status = keyboard.next();
+
+        if ( place.equals("outside") && status.equals("yes")){
+            System.out.println("\nIt is a bison, isn't it?");
+        }
+        if ( place.equals("outside") && status.equals("no")){
+            System.out.println("\nIt is a billboard, isn't it?");
+        }
+        if ( place.equals("both") && status.equals("yes")){
+            System.out.println("\nIt is a dog, isn't it?");
+        }
+        if ( place.equals("both") && status.equals("no")){
+            System.out.println("\nIt is a cell phone, isn't it?");
+        }
+        if ( place.equals("inside") && status.equals("yes")){
+            System.out.println("\nIt is a houseplant, isn't it?");
+        }
+        if ( place.equals("inside") && status.equals("no")){
+            System.out.println("\nIt is a shower curtain, isn't it?");
+        }
+    }
+}
