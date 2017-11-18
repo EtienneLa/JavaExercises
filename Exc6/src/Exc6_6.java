76d1e4e96fc0ebe98bc8475eb7e175938187d979
Unknown <etienne.langensiepen@gmx.de>
2017-11-18 15:08:46 +0100

    .compareTo()

---
+++ Exc6/src/Exc6_6.java
@@ -0,0 +1,21 @@
+public class Exc6_6 {
+    public static void main(String[]args){
+        String s1 = "Alexandra";
+        String s2 = "Alexander";
+        String s3 = "ball";
+        String s4 = "football";
+        String s5 = "abcde";
+        String s6 = "defgh";
+        int i1, i2, i3;
+
+        i1=s2.compareTo(s1);
+        System.out.println(i1);
+
+        i2=s4.compareTo(s3);
+        System.out.println(i2);
+
+        i3=s5.compareTo(s6);
+        System.out.println(i3);
+
+    }
+}
