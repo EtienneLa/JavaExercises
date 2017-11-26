cb971089913339b845c79e2604313ab7d4b8088e
Unknown <etienne.langensiepen@gmx.de>
2017-11-26 15:10:20 +0100

    Exc11_6/7

---
+++ Exc11 for loops/src/Exc11_6.java
@@ -0,0 +1,13 @@
+public class Exc11_6 {
+
+    public static void main(String[]args){
+
+        for ( int n = 1; n <= 20; n = n+1){
+
+            if ( (n % 2) == 0 )
+                System.out.println(n + "<");
+            else
+                System.out.println(n);
+        }
+    }
+}
