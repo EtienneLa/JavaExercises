4ecc9755869377832e1cfef002e13e3c938b7785
Unknown <etienne.langensiepen@gmx.de>
2017-12-08 17:39:12 +0100

    Exc14_2

--- Exc14 refresher/src/Exc14_2.java
+++ Exc14 refresher/src/Exc14_2.java
@@ -1,4 +1,23 @@
-import
+import java.util.Scanner;
 
 public class Exc14_2 {
+
+    public static void main(String[] args){
+
+        Scanner kb = new Scanner(System.in);
+
+        String name;
+
+        System.out.print("What is your name? ");
+        name = kb.next();
+        System.out.println();
+
+        for ( int i = 1; i <= 10; i++ ){
+
+            if ( name.equals("Etienne")){
+                i++;
+            }
+            System.out.println( name );
+        }
+    }
 }
