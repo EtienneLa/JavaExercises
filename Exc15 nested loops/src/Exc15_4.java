83a1e31e34386a76cbc3f0826ce69bbf7ed742f4
Unknown <etienne.langensiepen@gmx.de>
2017-12-08 19:10:33 +0100

    Exc15_4

---
+++ Exc15 nested loops/src/Exc15_4.java
@@ -0,0 +1,14 @@
+public class Exc15_4 {
+
+    public static void main( String[] args ){
+
+        for ( int i = 0; i <= 5; i++ ){
+
+            for ( int d = 0; d <= 5; d++ ){
+
+                System.out.print(" " + "(" + i + "|" + d + ") ");
+            }
+            System.out.println();
+        }
+    }
+}
