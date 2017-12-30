d3b7777fe8e38b8aa664ffbe0994607b5fbf4097
Unknown <etienne.langensiepen@gmx.de>
2017-12-30 16:05:34 +0100

    Exc17_9

---
+++ Exc17 arrays/src/Exc17_9.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Exc17_9 {
+
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+        Random rng = new Random();
+        int found = 0;
+
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+
+            int r = 1 + rng.nextInt(50);
+            array[i] = r;
+        }
+        System.out.print("Value to find: ");
+        int find = kb.nextInt();
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (find == array[i]) {
+
+                found = found + 1;
+            }
+        }
+        if (found != 0)
+            System.out.println("\n" + find + " was found " + found + " times.");
+        else
+            System.out.println("\n" + find + " is not in the array.");
+
+        System.out.print("\nArray:");
+
+        for (int i = 0; i < array.length; i++) {
+
+            System.out.print(" " + array[i]);
+        }
+    }
+}
