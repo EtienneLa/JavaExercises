83885d5b6e37755e8bf5f00a9f440b8d942ff6a1
Unknown <etienne.langensiepen@gmx.de>
2018-01-01 23:31:52 +0100

    Exc17_13

---
+++ Exc17 arrays/src/Exc17_13.java
@@ -0,0 +1,16 @@
+public class Exc17_13 {
+
+    public static void main(String[] args) {
+
+        String[] arr1 = {"alpha", "bravo", "charlie", "delta", "echo"};
+        int[] arr2 = {11, 23, 37, 41, 53};
+
+        for (int i = 0; i < arr1.length; i++){
+            System.out.print(arr1[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < arr2.length; i++){
+            System.out.print(arr2[i] + " ");
+        }
+    }
+}
