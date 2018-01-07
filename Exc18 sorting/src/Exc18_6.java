160965bd30e9d5e9995032347f560c6f58d52305
Unknown <etienne.langensiepen@gmx.de>
2018-01-07 23:08:20 +0100

    Exc18_7

--- Exc18 sorting/src/Exc18_6.java
+++ Exc18 sorting/src/Exc18_6.java
@@ -1,33 +1,27 @@
-public class Exc18_6
-{
-    public static void main( String[] args )
-    {
-        int[] arr = { 45, 87, 39, 32, 93, 86, 12, 44, 75, 50 };
+public class Exc18_6 {
+    public static void main(String[] args) {
+        int[] arr = {45, 87, 39, 32, 93, 86, 12, 44, 75, 50};
+        //int slotadd = 0;
 
-        // Display the original (unsorted values)
         System.out.print("before: ");
-        for ( int i=0; i<arr.length; i++ )
-            System.out.print( arr[i] + " " );
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
         System.out.println();
 
-		for ( int i = 0; i < arr.length; i++ )
-		{
-			for ( int j = 1; j < arr.length; j++)
-			{
-				if ( arr[j] > arr[i] )
-				{
-					int swap = arr[i];
-					arr[i] = arr[j];
-					arr[j] = swap;
-				}
-			}
-		}
+        for (int i = 0/*slotadd*/; i < arr.length; i++){
+            for (int j = 0; j < arr.length; j++){
 
+                if (arr[j] < arr[i]){
+                    int swap = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = swap;
+                    //slotadd++;
+                }
+            }
+        }
+        System.out.println("after: ");
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
 
-        // Display the values again, now (hopefully) sorted.
-        System.out.print("after : ");
-        for ( int i=0; i<arr.length; i++ )
-            System.out.print( arr[i] + " " );
-        System.out.println();
     }
 }
