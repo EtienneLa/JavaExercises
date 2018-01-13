43a002a3316449091c1bb79f1aa4d4c794567245
Unknown <etienne.langensiepen@gmx.de>
2018-01-13 16:26:25 +0100

    Exc19_5

--- Exc 19 records/src/Exc19_5.java
+++ Exc 19 records/src/Exc19_5.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
+class Studentdb {
+
+    String name;
+    int grade;
+    double avrg;
+
+    public String toString() {
+
+        return name + ", " + grade + ", " + avrg;
+    }
+}
+
 public class Exc19_5 {
+
+    public static void main( String[] args ) throws Exception {
+
+        Scanner kb = new Scanner(System.in);
+        Studentdb[] student = new Studentdb[3];
+
+        for ( int i = 0; i < student.length; i++ ) {
+            student[i] = new Studentdb();
+            System.out.print("Enter student " + (i+1) + "'s name: ");
+            student[i].name = kb.next();
+            System.out.print("Enter student " + (i+1) + "'s grade: ");
+            student[i].grade = kb.nextInt();
+            System.out.print("Enter student " + (i+1) + "'s average: ");
+            student[i].avrg = kb.nextDouble();
+        }
+        System.out.println();
+
+        for ( int i = 0; i < student.length; i++ ) {
+            System.out.println(i+1 + ". " + student[i]);
+        }
+    }
 }
