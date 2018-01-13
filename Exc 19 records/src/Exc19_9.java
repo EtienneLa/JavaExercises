d487aea6371ec47b7c11f401f6042a520d795c61
Unknown <etienne.langensiepen@gmx.de>
2018-01-13 17:41:26 +0100

    Exc19_9 not done

---
+++ Exc 19 records/src/Exc19_9.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+class Cars {
+
+    String name;
+    String model;
+    int year;
+    String license;
+}
+
+public class Exc19_9 {
+
+    public static void main( String[] args ) {
+
+        
+    }
+}
