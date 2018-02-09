17bebdc1a1192b2e41c7cfe548d9c73443eb5408
Unknown <etienne.langensiepen@gmx.de>
2018-02-09 16:37:57 +0100

    Exc22_replace/ulCase/trim

--- Exc22 StringMethods/src/StringMethod_2.java
+++ Exc22 StringMethods/src/StringMethod_2.java
@@ -18,5 +18,22 @@ public class StringMethod_2 {
         System.out.println(a.concat(b));
         String c = a.concat(b);
         System.out.println(c);
+
+        //replace Method
+
+        System.out.println(b.replace('b', 'h'));
+        System.out.println(b);
+
+        //upperCase / lowerCase Method
+
+        String d = "hEllO";
+        System.out.println(d.toUpperCase());
+        System.out.println(d.toLowerCase());
+
+        //trim Method removes spaces
+
+        String e = " W h            a t 's U          p";
+        e = e.trim();
+        System.out.println(e); //not working tho ??
     }
 }
