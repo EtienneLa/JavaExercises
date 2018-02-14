2ceece2ffdd79140d2d4adf9596fe90c1cb49c9f
Unknown <etienne.langensiepen@gmx.de>
2018-02-14 16:20:53 +0100

    Collections reverse/copy method

--- Exc23 some methods/src/Exc23_CollectionsMethod.java
+++ Exc23 some methods/src/Exc23_CollectionsMethod.java
@@ -4,6 +4,7 @@ public class Exc23_CollectionsMethod {
 
     public static void main(String[] args) {
 
+        //Collection sorting
         String[] words = {"apples", "lemons", "bacon", "bananas", "stuff"};
         List<String> list = Arrays.asList(words);
 
@@ -12,5 +13,32 @@ public class Exc23_CollectionsMethod {
 
         Collections.sort(list, Collections.reverseOrder());
         System.out.printf("%s", list);
+        System.out.println();
+
+
+        //reverse / copy methods
+        Character[] cray = {'a', 'n', 't', 'b'};
+        List<Character> clist = Arrays.asList(cray);
+        System.out.println("List is:");
+        output(clist);
+
+        Collections.reverse(clist);
+        System.out.println("After reverse:");
+        output(clist);
+
+        Character[] cray2 = new Character[4];
+        List<Character> clist2 = Arrays.asList(cray2);
+
+        Collections.copy(clist2, clist);
+        output(clist2);
+
+
+    }
+
+    public static void output(List<Character> list) {
+
+        for (char x : list)
+            System.out.printf("%s ", x);
+        System.out.println();
     }
 }
